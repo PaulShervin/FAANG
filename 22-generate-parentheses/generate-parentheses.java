f7e6dfbb28fc35161a5b54d68b4c7b7546629e96
PaulShervin <paulshervin77@gmail.com>
2026-03-03 22:44:57 +0530

    Time: 2 ms (68.29%) | Memory: 44.6 MB (71.46%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> answer=new ArrayList<>();
+
+        dfs(0,0,"",n,answer);
+        return answer;
+    }
+    public void dfs(int open_bracket,int close_bracket,String str,int n,List<String> answer)
+    {
+        if(open_bracket==close_bracket && open_bracket+close_bracket==n*2)
+        {
+            answer.add(str);
+        }
+        if(open_bracket<n)
+        {
+            dfs(open_bracket+1,close_bracket,str+"(",n,answer);
+        }
+        if(close_bracket<open_bracket){
+            dfs(open_bracket,close_bracket+1,str+")",n,answer);
+        }
+    }
+}
